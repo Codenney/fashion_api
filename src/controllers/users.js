218884b3368d24fc5c63fc0f2d65d74d46900069
Olaitan <kenney9224@gmail.com>
2022-10-28 09:27:05 +0100

    Added an encryption to the user password before storing to the DB

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,14 +1,21 @@
-const express = require('express');
 const db = require('../../config/database');
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 
 exports.createUser = async (req, res) => {
     try {
-        const {name, email, password} = req.body;
+        let {name, email, password} = req.body;
         // const userEmail = await db.one('SELECT * FROM users WHERE email = $1', email);
         if(email) {
             const userName = (await db.oneOrNone('SELECT name FROM users WHERE email = $1', email));
             if(userName) throw new Error("User with the provide details exist");
         }
+        password = await bcrypt.hash(password, 12);
+        const token = jwt.sign({userEmail: email}, process.env.JWT_SECRET, {expiresIn: process.env.EXPIRES_IN});
+        console.log(token);
         const newUser = await db.one('INSERT INTO users(name, email, password) VALUES(${name}, ${email}, ${password}) RETURNING id', {
             name: name, 
             email: email, 
