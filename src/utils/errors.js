7579e2248c923d7d2f9c4b42ecf50b6996eb6f51
Olaitan <kenney9224@gmail.com>
2022-10-29 22:30:04 +0100

    Still trying to handle the errors properly

---
+++ src/utils/errors.js
@@ -0,0 +1,23 @@
+class CustomError extends Error {
+    constructor({statusCode, message}){
+        super();
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+
+const handleError = (err, req, res, next) => {
+    let {statusCode, message } = err;
+    if(!statusCode) statusCode = 500;
+
+    res.status(statusCode).json({
+        status: 'error',
+        statusCode,
+        message
+    })
+}
+
+module.exports = {
+    CustomError,
+    handleError
+};
